9ed1bd8ade2b7ed14a64555a9c6a4ff186a398aa
janainafortunato <janaina.projetos@live.com>
2021-01-18 17:33:18 -0300

    Criação da tabela cardapio

---
+++ app/Http/Controllers/CardapioController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Cardapio;
+use Illuminate\Http\Request;
+
+class CardapioController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Cardapio  $cardapio
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Cardapio $cardapio)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Cardapio  $cardapio
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Cardapio $cardapio)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Cardapio  $cardapio
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Cardapio $cardapio)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Cardapio  $cardapio
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Cardapio $cardapio)
+    {
+        //
+    }
+}
