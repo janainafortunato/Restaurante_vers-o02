0a8f7dcca6aa0f42536d62af193d241c630e7061
janainafortunato <janaina.projetos@live.com>
2021-01-20 22:11:58 -0300

    Inserindo atributos na class users cep_cpf

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,6 +19,9 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('endereco')->nullable();
+            $table->string('cep');
+            $table->string('cpf')->unique();
             $table->rememberToken();
             $table->timestamps();
         });
